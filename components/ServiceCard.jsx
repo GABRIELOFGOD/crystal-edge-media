8c79c1731f7b7615a1ea904145eac87df5248b0c
khodijah0498 <khodijahlawal@gmail.com>
2024-03-18 16:15:33 +0100

    feat: frontend

--- components/ServiceCard.jsx
+++ components/ServiceCard.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const ServiceCard = ({icon, name, path, def}) => {
   return (
-    <div className='rounded-md border w-[400px] h-[200px] p-3 border-light bg-[#ffffff]  gap-3'>
+    <div className='rounded-md border w-[400px] h-[250px] p-3 border-light bg-[#ffffff]  gap-3'>
         <span className=' text-4xl bg-light h-72 w-72 '>{icon}</span>        
-        <div className=" flex flex-col gap-4">
+        <div className=" flex flex-col gap-2">
             <p className="text-xl font-semibold text-black">{name}</p>
-            <p className='text-base'>{def}</p>
+            <p className='text-sm font-light'>{def}</p>
 
             {/* <div className="text-[10px] flex justify-end text-light items-end">Know more...</div> */}
         </div>
