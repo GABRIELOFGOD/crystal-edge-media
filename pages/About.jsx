fec4a950fad523ac402689c43e34f051e8922b40
GABRIELOFGOD <gabrielayodele2021@gmail.com>
2024-03-23 18:46:08 +0100

    It the weekend

--- pages/About.jsx
+++ pages/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Newsletter from '../components/Newsletter';
 import ShortService from '../components/ShortService';
+import { Link } from 'react-router-dom';
 const aboutImage = '/images/about-image-min.jpg'
 
 const About = () => {
@@ -9,9 +10,9 @@ const About = () => {
       <div className='px-6 md:px-24'>
         <p className="text-center font-bold text-3xl py-5">We are <span className="text-primary">Crystal Edge Media</span></p>
         <div className="flex md:grid grid-cols-2 md:gap-10 gap-5 justify-between">
-          <div className='my-auto'>
+          <div className='my-auto flex flex-col gap-5'>
             <p className='text-lg'><span className="text-light font-semibold">CrystalEdge Media Limited </span> provides public relations and media relations services for brands, organizations, and individuals. We elevate brands and assist organizations and individuals in garnering positive media exposure and bolster their public reputation. Through a blend of expertise, innovation, and strategic approach, we work to become the trusted partner of our valued clients and deliver tangible results for them</p>
-            <button className='bg-primary text-white py-3 px-6 rounded-md mt-10'>Get in Touch</button>
+            <Link target='blank' to='https://wa.me/2348070554185' className='bg-primary w-fit text-white py-3 px-6 rounded-md'>Get in Touch</Link>
           </div>
           <img src={aboutImage} className='w-fit hidden md:flex h-[80%] my-auto' alt="about-image" />
         </div>
